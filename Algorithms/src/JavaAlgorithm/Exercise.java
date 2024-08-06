a61893a7aebfa81d38abc33097408e49e5a34de2
unknown <thaonguyen20312@gmail.com>
2024-08-06 17:06:56 +0700

    add fuction

--- Algorithms/src/JavaAlgorithm/Exercise.java
+++ Algorithms/src/JavaAlgorithm/Exercise.java
@@ -5,5 +5,8 @@ public static void main(String[] args) {
 	System.out.println("Hello");
 	System.out.println("Hello World");
 	System.out.println("Hello 123");
+	int a = 1;
+	int b = 5;
+	System.out.println("Total" + " " + a+b);
 }
 }
